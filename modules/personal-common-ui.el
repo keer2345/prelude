ea916e769b0c01dbbf5a36e1715bb7ae1801f5b2
keer2345 <keer2345@gmail.com>
2023-12-26 23:10:36 +0800

    all-the-icons

--- modules/personal-common-ui.el
+++ modules/personal-common-ui.el
@@ -1,9 +1,12 @@
 ;;
 ;; M-x customize-themes 
 
-(prelude-require-packages '(dracula-theme doom-themes))
+(prelude-require-packages 
+	'(doom-themes 
+	  dracula-theme))
 ;; (load-theme 'dracula t)
 
-
+(use-package all-the-icons
+  :if (display-graphic-p))
 
 (provide 'personal-common-ui)
