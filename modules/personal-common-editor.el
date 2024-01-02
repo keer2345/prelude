f2d533078f1c8ddd4125879efe6d824cb760d289
keer2345 <keer2345@gmail.com>
2024-01-02 23:39:56 +0800

    defun forward-kill-whitespace-or-word ()

--- modules/personal-common-editor.el
+++ modules/personal-common-editor.el
@@ -3,6 +3,21 @@
 	(("C-a" . mwim-beginning-of-code-or-line)
 	 ("C-e" . mwim-end-of-code-or-line)))
 
+(defun forward-kill-whitespace-or-word ()
+  "If `point' is followed by whitespace kill that.
+Otherwise call `kill-word'"
+  (interactive)
+  (if (looking-at "[ \t\n]")
+      (let ((pos (point)))
+        (re-search-forward "[^ \t\n]" nil t)
+        (backward-char)
+        (kill-region pos (point)))
+    (kill-word 1)))
+(global-set-key (kbd "M-d") 'forward-kill-whitespace-or-word)
+
+(setq whitespace-line-column 120) ;; limit line length
+
+
 (global-set-key (kbd "C-z") 'undo)
 
 (provide 'personal-common-editor)
