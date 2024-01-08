549825bb1ecbf3bd92c04f00299e8210a40031b3
keer2345 <keer2345@gmail.com>
2024-01-08 21:20:04 +0800

    set Fira Code font

--- modules/personal-common-ui.el
+++ modules/personal-common-ui.el
@@ -15,4 +15,13 @@
 (use-package fira-code-mode
   :config (global-fira-code-mode))
 
+
+(if (display-graphic-p)
+	(progn
+		(add-to-list 'default-frame-alist '(width . 124))  ; 设定启动图形界面时的初始 Frame 宽度（字符数）
+		(add-to-list 'default-frame-alist '(height . 40))  ; 设定启动图形界面时的初始 Frame 高度（字符数）
+	))
+
+(set-face-attribute 'default nil :font "Fira Code Retina")
+
 (provide 'personal-common-ui)
--- personal/preload/preload-package.el
+++ personal/preload/preload-package.el
@@ -13,7 +13,7 @@
 
 ;; if want to connect host raw.githubusercontent.com , we could eanble:
 ;;
-(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ; 不加这一句可能有问题，建议读者尝试一下
-(setq url-proxy-services '(("no_proxy" . "^\\(192\\.168\\..*\\)")
-                     ("http" . "127.0.0.1:8889")
-                     ("https" . "127.0.0.1:8889")))
+;; (setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3") ; 不加这一句可能有问题，建议读者尝试一下
+;; (setq url-proxy-services '(("no_proxy" . "^\\(192\\.168\\..*\\)")
+;;                      ("http" . "127.0.0.1:8889")
+;;                      ("https" . "127.0.0.1:8889")))
